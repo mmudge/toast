39250fa0ce8c2f06c60da9c7730b2c27c564909a
michael mudge <michaeltmudge@gmail.com>
2021-02-15 17:56:54 -0800

    basic menu items set up

--- src/store/index.ts
+++ src/store/index.ts
@@ -5,7 +5,47 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    showNavDrawerState: false
+    showNavDrawerState: false,
+    menuItemsState: [
+      {
+        type: 'Hot Drinks',
+        items: [
+          { name: 'Black Coffee', price: '3' },
+          { name: 'Americano', price: '3' },
+          { name: 'Latte', price: '3.5' },
+          { name: 'Cappuccino', price: '3.5' },
+          { name: 'Flat White', price: '4.5' },
+          { name: 'Drip Coffee', price: '5' }
+        ]
+      },
+      {
+        type: 'Cold Drinks',
+        items: [
+          { name: 'Iced Coffee', price: '3' },
+          { name: 'Cold Brew', price: '3.5' },
+          { name: 'Iced Tea', price: '3' },
+          { name: 'Vanilla Cream', price: '4' }
+        ]
+      },
+      {
+        type: 'Breakfast',
+        items: [
+          { name: 'Toast of the Day', price: '6' },
+          { name: 'Eggs and Toast', price: '9' },
+          { name: 'Avocado Toast', price: '11.5' },
+          { name: 'French Toast', price: '9.5' }
+        ]
+      },
+      {
+        type: 'Lunch',
+        items: [
+          { name: 'Grilled Cheese', price: '9' },
+          { name: 'Chili Cheese Toast', price: '10.5' },
+          { name: 'Chicken Avocado Sandwhich', price: '14' },
+          { name: 'Spinach Kale Salad', price: '14' }
+        ]
+      }
+    ]
   },
   mutations: {
     setShowNavDrawer(state, value) {
@@ -17,6 +57,9 @@ export default new Vuex.Store({
   getters: {
     showNavDrawer: state => {
       return state.showNavDrawerState
+    },
+    menuItems: state => {
+      return state.menuItemsState
     }
   }
 })
--- src/views/Menu.vue
+++ src/views/Menu.vue
@@ -1,5 +1,43 @@
 <template>
-  <div><h1>Menu</h1></div>
+  <div class="medium-container py-16">
+    <v-layout>
+      <v-flex class="pr-16">
+        <h1 class="text-h3 font-weight-regular">Drinks</h1>
+        <h2 class="pt-16 accent--text text-h4">Hot</h2>
+        <template v-for="item in hotDrinkItems.items">
+          <v-layout justify-space-between pt-5 :key="item.name">
+            <h3 class="text-h5">{{ item.name }}</h3>
+            <h3 class="text-h5">{{ item.price }}</h3>
+          </v-layout>
+        </template>
+        <h2 class="pt-16 accent--text text-h4">Cold</h2>
+        <template v-for="item in coldDrinkItems.items">
+          <v-layout justify-space-between pt-5 :key="item.name">
+            <h3 class="text-h5">{{ item.name }}</h3>
+            <h3 class="text-h5">{{ item.price }}</h3>
+          </v-layout>
+        </template>
+      </v-flex>
+      <v-flex class="pl-16">
+        <h1 class="text-h3 font-weight-regular">Food</h1>
+        <h2 class="pt-16 accent--text text-h4">Breakfast</h2>
+        <template v-for="item in breakfastItems.items">
+          <v-layout justify-space-between pt-5 :key="item.name">
+            <h3 class="text-h5">{{ item.name }}</h3>
+            <h3 class="text-h5">{{ item.price }}</h3>
+          </v-layout>
+        </template>
+
+        <h2 class="pt-16 accent--text text-h4">Lunch</h2>
+        <template v-for="item in lunchItems.items">
+          <v-layout justify-space-between pt-5 :key="item.name">
+            <h3 class="text-h5">{{ item.name }}</h3>
+            <h3 class="text-h5">{{ item.price }}</h3>
+          </v-layout>
+        </template>
+      </v-flex>
+    </v-layout>
+  </div>
 </template>
 
 <script lang="ts">
@@ -7,7 +45,31 @@ import { Component } from 'vue-property-decorator'
 import AppComponent from '@/components/AppComponent'
 
 @Component
-export default class Menu extends AppComponent {}
+export default class Menu extends AppComponent {
+  get hotDrinkItems() {
+    return this.$store.getters.menuItems.find(
+      (item: any) => item.type.toLowerCase() === 'hot drinks'
+    )
+  }
+
+  get coldDrinkItems() {
+    return this.$store.getters.menuItems.find(
+      (item: any) => item.type.toLowerCase() === 'cold drinks'
+    )
+  }
+
+  get breakfastItems() {
+    return this.$store.getters.menuItems.find(
+      (item: any) => item.type.toLowerCase() === 'breakfast'
+    )
+  }
+
+  get lunchItems() {
+    return this.$store.getters.menuItems.find(
+      (item: any) => item.type.toLowerCase() === 'lunch'
+    )
+  }
+}
 </script>
 
 <style scoped></style>
